321d3fbd873b923367d9b4d11d1fa7ec2aef6571
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2019-01-28 22:27:42 +0100

    Changed date format tooltip

--- src/components/FlippedSentence/index.jsx
+++ src/components/FlippedSentence/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import ReactTimeAgo from 'react-time-ago/no-tooltip'
 import defaultStyle from 'javascript-time-ago/commonjs/style/default'
+import { format as dateFormat } from 'date-fns'
 
 import './flipped-sentence.css'
 
@@ -20,10 +21,11 @@ export default class FlippedSentence extends Component {
 
     render() {
         const { sentence, created } = this.props.sentence;
+        const dateTime = new Date(created);
 
         return (
             <div styleName={classNames('default', { visible: sentence.length > 0 })}>
-                {sentence.length > 0 && <ReactTimeAgo styleName="created" date={new Date(created)} timeStyle={defaultStyleShort} />}
+                {sentence.length > 0 && <ReactTimeAgo styleName="created" date={dateTime} formatVerboseDate={_ => dateFormat(dateTime, 'YYYY-MM-DD HH:mm:ss Z')} timeStyle={defaultStyleShort} />}
                 <div>
                     {sentence}
                 </div>
--- src/components/LastSentences/index.jsx
+++ src/components/LastSentences/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactTimeAgo from 'react-time-ago/no-tooltip'
 import defaultStyle from 'javascript-time-ago/commonjs/style/default'
+import { format as dateFormat } from 'date-fns'
 
 import './last-sentences.css';
 
@@ -23,7 +24,7 @@ export default class LastSentences extends Component {
     return (
       <div styleName="container">{
           sentences.map(s => (<div key={s.id} styleName="sentence">
-                                <ReactTimeAgo styleName="created" date={new Date(s.created)} timeStyle={defaultStyleShort} />
+                                <ReactTimeAgo styleName="created" date={new Date(s.created)} formatVerboseDate={_ => dateFormat(new Date(s.created), 'YYYY-MM-DD HH:mm:ss Z')} timeStyle={defaultStyleShort} />
                                 <div>
                                   {s.sentence}
                                 </div>
