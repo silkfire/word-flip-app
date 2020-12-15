c458b1532542ff549ac5ef90b5938e726f1f5ad9
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2020-12-15 19:15:53 +0100

    Bump packages

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -34,7 +34,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        { from: pathJoinUnix(srcPath, 'favicon.*'), to: distPath, flatten: true },
+        { from: pathJoinUnix(srcPath, 'favicon.*'), to: `${distPath}/[name].[ext]` },
       ],
     }),
     new HtmlWebpackPlugin({
